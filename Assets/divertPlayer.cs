26c96b1712f879166a5f63dd4c229a27ceb16e6a
Andreas Graham <s179055@students.aie.edu.au>
2018-02-05 12:19:59 -0800

    thief basics

---
+++ Assets/divertPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class divertPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponent<pathTest>();
+        player.firstCollision++;
+        //need one object of a collision to have a rigidbody
+    }
+}
---
+++ Assets/pathTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class pathTest : MonoBehaviour {
+
+    NavMeshAgent nav;
+    public GameObject artPiece;
+    public GameObject artPiece2;
+    public float firstCollision;
+
+	void Start () {
+        nav = GetComponent<NavMeshAgent>();
+        firstCollision = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(firstCollision == 1)
+        {
+            nav.destination = artPiece.transform.position;
+            nav.SetAreaCost(3, nav.GetAreaCost(3) + 1);
+            Debug.Log(nav.GetAreaCost(3));
+        }
+        else if(firstCollision == 2)
+        {
+            nav.destination = artPiece2.transform.position;
+        }
+
+	}
+}

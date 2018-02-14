9a843fbe46370d6dcb862ab6327bfeae4c27ed66
Cameron Zach P Tucker <s179071@students.aie.edu.au>
2018-02-14 10:13:20 -0800

    added reset

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+
+    public string thisSceneName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<pathTester>() != null)
+        {
+            SceneManager.LoadScene(thisSceneName);
+        }
+    }
+}
